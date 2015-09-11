6c169083093fa6233952271a8af4a9feacc236e5
Matt Price <matt.price@utoronto.ca>
2015-09-10 22:13:06 -0400

    Remove annoying footer menu

--- footer.php
+++ footer.php
@@ -8,14 +8,14 @@
 </div>
 
 <footer class="full-width" role="contentinfo">
-	<div class="row">
-		<div class="large-12 columns">
-			<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
-		</div>
-	</div>
+    <!-- <div class="row">
+    <div class="large-12 columns">
+    <?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
+    </div>
+    </div> -->
 	<div class="row love-reverie">
 		<div class="large-12 columns">
-			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('Made with Love in','grunterie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
+			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('Made with Love in','grunterie'); ?> <a href="http://github.com/gpspake/grunterie/" rel="nofollow" title="Reverie Framework">Grunterie</a>.</p>
 		</div>
 	</div>
 </footer>
